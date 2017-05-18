cc647ab50a72da3e8607109fa611e5622d9766d3
Lu Tam Long <lulong.dak@gmail.com>
2017-05-18 23:54:14 +0700

    Create project and add Google Firebase to project

---
+++ Firebase_Example/Firebase_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase_Example
+//
+//  Created by Lu Tam Long on 5/18/17.
+//  Copyright © 2017 Lu Tam Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
